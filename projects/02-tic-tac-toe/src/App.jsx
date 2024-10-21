f5b6e517e333277fbadba47e2ec0e323626ccaaf
YulianaCV <yulivcrvs05@gmail.com>
2024-10-21 10:13:28 -0600

    "iniciando proyecto 2 juego"

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -1,51 +1,25 @@
-import { Children } from "react"
-import { useState } from "react"
-
 const TURNS = {
-  X:'X',
-  O:'O'
+  X:'x',
+  O:'o'
 }
 
-const Square = ({Children, isSelected, updateBoard, index}) =>{
-  const className = `square ${isSelected ? 'is-selected': ''}`
-  return(
-    <div className={className}>
-      {Children}
-    </div>
-  )
-}
+const board = Array(9).fill(null)
 
 function App(){
-  const [board,setBoard] = useState( Array(9).fill(null))
-
-  const {turn, setTurn} = useState(TURNS.X)
-
   return(
     <main className="board">
-        <h1>Tic tac toe</h1>
-        <section className="game">
-          {
-            board.map((_, index) => {
-              return(
-                <Square
-                key={index}
-                index={index}
-                >
-                  {board[index]}
-                </Square>
-              )
-            })
-          }
-        </section>
+      <h1>Tic tac toe</h1>
+      <section className="game">
+        {
+          board.map((_, index) {
 
-        <section className="turn">
-          <Square isSelected={turn}>
-            {TURNS.X}
-          </Square>
-          <Square>
-            {TURNS.O}
-          </Square>
-        </section>
+          }
+
+
+
+        )
+        }
+      </section>
     </main>
   ) 
 }
