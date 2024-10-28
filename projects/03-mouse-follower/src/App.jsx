7780bd712f89653b92d80770f15a0a37480acae9
YulICV <yulivcrvs05@gmail.com>
2024-10-28 17:01:41 -0600

    actualizacion proyecto 03

--- projects/03-mouse-follower/src/App.jsx
+++ projects/03-mouse-follower/src/App.jsx
@@ -1,34 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useEffect,useState } from 'react'
+
+const FollowMouse= () => {
+  const[enabled, setEnabled] = useState(false)
+  const[position, setPosition] = useState({x:0, y:0})
+
+  useEffect(() => {
+    console.log('effect', {enabled})
+     
+    const handleMove = (event) => {
+      const {clientX, clientY} = event
+      console.log('handleMove', {clientX,clientY})
+      setPosition({x: clientX, y: clientY})
+    }
+
+    if(enabled){
+      window.addEventListener('pointermove', handleMove)
+    }
+  
+    //cleanup
+    return () => {
+      console.log('cleanup')
+      window.removeEventListener('pointermove', handleMove)
+    }
+  }, [enabled])
+  
+  return(
+    <>
+    <div  style={{
+      position: 'absolute',
+      backgroundColor:'#09f',
+      borderRadius:'50%',
+      opacity: 0.8,
+      pointerEvents: 'none',
+      left: -20,
+      top: -20,
+      width: 40,
+      height:40,
+      transform:`translate(${position.x}px,${position.y}px)`
+    }}/>
+
+    <button onClick={()=> setEnabled(!enabled)}>
+      {enabled?'Desactivar':'Activar'} seguir puntero
+    </button>
+  </>
+  )
+}
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [mounted,setMounted] = useState(true)
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main>
+      {mounted && <FollowMouse/>}
+      <button onClick={() =>setMounted(!mounted)}>Toggle mounted component</button>
+    </main>
   )
 }
 
